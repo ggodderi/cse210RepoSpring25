2fc7144e95e78779ba2efadd21edcfcef72f1cc8
Gary Godderidge <102984600+ggodderi@users.noreply.github.com>
2025-04-24 11:12:06 -0600

    Initial for name assignemnt.

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,8 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        // Console.WriteLine("Hello Sandbox World!");
         // This is a test.
         // This is a second change.
+
+        Console.Write("Input your first name: ");
+        string firstName = Console.ReadLine();
+        Console.Write("Input your Last name: ");
+        string lastName = Console.ReadLine();
+        Console.WriteLine($"Your name is: {lastName}, {firstName} {lastName}");
+
     }
 }
