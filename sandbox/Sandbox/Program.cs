b277f6694f13113f1c6c98fe9c7661d7920a8666
Gary Godderidge <102984600+ggodderi@users.noreply.github.com>
2025-05-01 10:24:46 -0600

    Removed some code

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -7,10 +7,6 @@ class Program
         return a + b;
     }
 
-    static void DisplayGreeting()
-    {
-        Console.WriteLine("Hello Bob, how are you?");
-    }
     static void Main(string[] args)
     {
         // Console.WriteLine("Hello Sandbox World!");
@@ -35,42 +31,11 @@ class Program
 
         // Console.WriteLine("I am doing great.");
 
-        // for(int i = 0; i < 20; i++)
-        // {
-        //     Console.WriteLine($"Hello Bob {i}");
-        // }
+        for(int i = 0; i < 20; i++)
+        {
+            Console.WriteLine($"Hello Bob {i}");
+        }
 
-        // int x = 0;
-        // x++;
-        // int y = x;
-        // int y = x++;
-        // Console.WriteLine(y);
-        // Console.WriteLine(x);
-        // y = ++x;
-        // Console.WriteLine(y);
-        // x--;
-        // Console.WriteLine($"{x++}");
-        // Console.WriteLine(x);
-
-        // List<int> myNumbers = new List<int>();   
-        // myNumbers.Add(10);
-        // myNumbers.Add(99);
-        // myNumbers.Add(-123);
-        // int total = 0;
-
-        // foreach(int i in myNumbers)
-        // {
-        //     Console.WriteLine(i);
-        //     total += i;
-        // }
-
-        // Console.WriteLine(total);
-
-        double total = AddNumbers(123.4546, 987.1234);
-        Console.WriteLine(total);
-        Console.WriteLine(AddNumbers(234, 432));
-
-        DisplayGreeting();
-
+    Console.WriteLine("");
     }
 }
