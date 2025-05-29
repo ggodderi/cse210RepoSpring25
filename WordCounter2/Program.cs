7167183685d32dad2214d1419e7bbf36a012a6f4
Gary Godderidge <102984600+ggodderi@users.noreply.github.com>
2025-05-29 09:13:33 -0600

    Created new projects for test and word counter.

---
+++ WordCounter2/Program.cs
@@ -0,0 +1,9 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        // Console.WriteLine("Bonjour tout le monde.");
+        WordCounter wordCounter = new WordCounter("This is a test sentence to allow testing my new class");
+        wordCounter.DisplayWords();
+    }
+}
