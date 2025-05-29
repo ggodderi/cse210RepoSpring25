63eaeb2effc0ba713814db376e4d4a53c95eb32b
Gary Godderidge <102984600+ggodderi@users.noreply.github.com>
2025-05-29 09:26:11 -0600

    Added CountSingleWord.

--- WordCounter/Program.cs
+++ WordCounter/Program.cs
@@ -4,7 +4,11 @@
     {
         Console.WriteLine("Hello world");
 
-        WordCounter wordCounter = new WordCounter("This is a text string that will test my counter class to see if things work well.");
+        WordCounter wordCounter = new WordCounter("This is a text string that will test my counter class to see if things work well text text.");
         wordCounter.DisplayWords();
+
+        int count = wordCounter.CountSingleWord("text");
+
+        Console.WriteLine($"The word: text, is found {count} times");
     }
 }
