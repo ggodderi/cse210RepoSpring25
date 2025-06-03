a3dfd318a281c3705b264a07442dc9601a13a51b
Gary Godderidge <102984600+ggodderi@users.noreply.github.com>
2025-06-03 11:03:07 -0600

    Created new Person2 project and updated previous person project.

--- Person/Program.cs
+++ Person/Program.cs
@@ -2,6 +2,12 @@
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Bob");
+        Person myPerson = new Person("Bubba", "Bob", 53);
+
+        Console.WriteLine(myPerson.GetPersonInformation());
+
+        PoliceMan myPoliceMan = new PoliceMan("Cooper", "Silver", 34, "Club");
+        Console.WriteLine(myPoliceMan.GetPersonInformation());
+
     }
 }
