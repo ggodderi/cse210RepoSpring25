746beb74173bc71702896faa60fa2ac0979bc4af
Gary Godderidge <102984600+ggodderi@users.noreply.github.com>
2025-06-17 08:28:53 -0600

    Added spinners to the Sandbox.

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel;
 using System.Diagnostics.Tracing;
 using System.IO.Compression;
 
@@ -7,30 +8,109 @@ class Program
 {
     private static void Main(string[] args)
     {
-        int sleepTime =450;
-        DateTime startTime = DateTime.Now;
-        DateTime futureTime = startTime.AddSeconds(20);
-        string animationString = "-/-\\_";
-        string animationString2 = ":(:)";
-        string animationString3 = "(-_-)(^_^)";
-        int count = 0;
 
-        Console.Write("Waiting: ");
-        while (DateTime.Now < futureTime)
+        // Console.WriteLine("Yo Bob");
+
+        int duration = 12;
+        DateTime currentTime = DateTime.Now;
+        DateTime endTime = currentTime.AddSeconds(duration);
+        int sleepTime = 150;
+        int count = duration;
+
+        string animationString2 = "\\|/-";
+        int index = 0;
+
+        // Spinner
+        while (DateTime.Now < endTime)
         {
-            // Console.Write(".");
-            // Console.Write("+");
-            Console.Write(animationString3[0..5]);
+            Console.Write(animationString2[index++ % animationString2.Length]);
             Thread.Sleep(sleepTime);
-            Console.Write("\b\b\b\b\b     \b\b\b\b\b");
-            Console.Write(animationString3[5..]);
-            Thread.Sleep(sleepTime);
-            Console.Write("\b\b\b\b\b     \b\b\b\b\b");
-            count += animationString3.Length;
-            // Console.Write("-");
-            // Thread.Sleep(sleepTime);
-            // Console.Write("\b \b");
+            Console.Write("\b");
         }
+
+        // Animate faces.
+        string animationString = "(^_^)(-_-)";
+
+        while (DateTime.Now < endTime)
+        {
+            Console.Write(animationString[0..5]);
+            Thread.Sleep(sleepTime);
+            Console.Write("\b\b\b\b\b");
+            Console.Write(animationString[5..]);
+            Thread.Sleep(sleepTime);
+            Console.Write("\b\b\b\b\b");
+ 
+        }
+
+        //Animate a count down.
+        while (DateTime.Now < endTime)
+        {
+            Console.Write(count--);
+            Thread.Sleep(1000);
+            if (count >= 9)
+                Console.Write("\b\b  \b\b");
+            else
+            {
+                Console.Write("\b");
+            }
+        }
+
+        // Animate + and -
+        while (DateTime.Now < endTime)
+        {
+            Console.Write("+");
+            Thread.Sleep(sleepTime);
+            Console.Write("\b");
+            Console.Write("-");
+            Thread.Sleep(sleepTime);
+            Console.Write("\b");
+
+        }
+      
+    }
+}
+
+
+  // int sleepTime = 250;
+        // int time = 9;
+
+        // DateTime currentTime = DateTime.Now;
+        // DateTime endTime = currentTime.AddSeconds(time);
+
+
+        // string animationString2 = "-+\\|/=)*&$";
+        // int index = 0;
+
+        // while (DateTime.Now < endTime)
+        // {
+        //     Console.Write(animationString2[index++ % animationString2.Length]);
+        //     Thread.Sleep(sleepTime);
+        //     Console.Write("\b");
+        // }
+
+        // int count = time;
+
+        // while (DateTime.Now < endTime)
+        // {
+        //     Console.Write(count--);
+        //     Thread.Sleep(1000);
+        //     Console.Write("\b");
+        // }
+
+        // string animationString = "(^_^)(-_-)";
+
+        // while (DateTime.Now < endTime)
+        // {
+        //     Console.Write(animationString[0..5]);
+        //     Thread.Sleep(sleepTime);
+        //     Console.Write("\b\b\b\b\b");
+        //     Console.Write(animationString[5..]);
+        //     // Console.Write("-");
+        //     Thread.Sleep(sleepTime);
+        //     Console.Write("\b\b\b\b\b");
+        //     // Console.Write("\n\t\\")
+
+        // }
         // Circle myCircle = new Circle(10);
         // // myCircle.SetRadius(10);
         // // myCircle._radius = -10;
@@ -39,12 +119,34 @@ class Program
         // // myCircle2.SetRadius(20);
         // // Console.WriteLine($"{myCircle2.GetRadius()}");
 
-            // Console.WriteLine($"{myCircle.GetArea()}");
-            // // Console.WriteLine($"{myCircle2.GetArea()}");
+        // Console.WriteLine($"{myCircle.GetArea()}");
+        // // Console.WriteLine($"{myCircle2.GetArea()}");
 
-            // Cylinder myCylinder = new Cylinder(100, 8);
-            // // myCylinder.SetHeight(10);
-            // // myCylinder.SetCircle(myCircle);
-            // Console.WriteLine($"{myCylinder.GetVolume()}");
-    }
-}
+        // Cylinder myCylinder = new Cylinder(100, 8);
+        // // myCylinder.SetHeight(10);
+        // // myCylinder.SetCircle(myCircle);
+        // Console.WriteLine($"{myCylinder.GetVolume()}");
+        //             int sleepTime =450;
+        // DateTime startTime = DateTime.Now;
+        // DateTime futureTime = startTime.AddSeconds(20);
+        // string animationString = "-/-\\_";
+        // string animationString2 = ":(:)";
+        // string animationString3 = "(-_-)(^_^)";
+        // int count = 0;
+
+        // Console.Write("Waiting: ");
+        // while (DateTime.Now < futureTime)
+        // {
+        //     // Console.Write(".");
+        //     // Console.Write("+");
+        //     Console.Write(animationString3[0..5]);
+        //     Thread.Sleep(sleepTime);
+        //     Console.Write("\b\b\b\b\b     \b\b\b\b\b");
+        //     Console.Write(animationString3[5..]);
+        //     Thread.Sleep(sleepTime);
+        //     Console.Write("\b\b\b\b\b     \b\b\b\b\b");
+        //     count += animationString3.Length;
+        //     // Console.Write("-");
+        //     // Thread.Sleep(sleepTime);
+        //     // Console.Write("\b \b");
+        // }
